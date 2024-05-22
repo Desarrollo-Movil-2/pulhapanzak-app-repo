d468828ab3cf329cbbd92cda5ed9424397bda21f
HaroruDa3 <harol.kock34@gmail.com>
2024-05-21 23:10:54 -0600

    x

--- pulhapanzak-app/src/main.ts
+++ pulhapanzak-app/src/main.ts
@@ -1,11 +1,19 @@
 import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import {
+  IonicRouteStrategy,
+  provideIonicAngular,
+} from '@ionic/angular/standalone';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 if (environment.production) {
   enableProdMode();
@@ -16,5 +24,19 @@ bootstrapApplication(AppComponent, {
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'pulha-app-616f1',
+        appId: '1:666562475134:web:77ed4906f7a807f917142f',
+        storageBucket: 'pulha-app-616f1.appspot.com',
+        apiKey: 'AIzaSyBe1E5udiE-kFbu8ZdwtmP6c0oAvc9Gqxg',
+        authDomain: 'pulha-app-616f1.firebaseapp.com',
+        messagingSenderId: '666562475134',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideMessaging(() => getMessaging()),
+    provideStorage(() => getStorage()),
   ],
 });
