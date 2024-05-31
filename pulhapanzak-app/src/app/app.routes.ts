93acb03434fd2feb9e04ef6dba52dea35e0688ce
HaroruDa3 <harol.kock34@gmail.com>
2024-05-31 13:09:05 -0600

    correcciones en el formulario de registro

--- pulhapanzak-app/src/app/app.routes.ts
+++ pulhapanzak-app/src/app/app.routes.ts
@@ -6,20 +6,8 @@ export const routes: Routes = [
     loadComponent: () => import('./auth/ui/pages/register/register.page').then( m => m.RegisterPage)
   },
   {
-<<<<<<< Updated upstream
-    path: 'tabs',
-    loadComponent: () => import('./shared/ui/pages/tabs/tabs.page').then( m => m.TabsPage)
-  },
-  {
-    path:'',
-    redirectTo:'login',
-    
-  }
-
-=======
     path: 'login',
     loadComponent: () => import('./auth/ui/pages/login/login.page').then( m => m.LoginPage)
   },
->>>>>>> Stashed changes
 
 ];
