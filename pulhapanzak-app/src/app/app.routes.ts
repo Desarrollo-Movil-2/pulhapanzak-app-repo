163e583ec57940058f2fb260c689214c22bad3be
HaroruDa3 <harol.kock34@gmail.com>
2024-05-31 13:04:18 -0600

    correcciones en el formulario

--- pulhapanzak-app/src/app/app.routes.ts
+++ pulhapanzak-app/src/app/app.routes.ts
@@ -3,9 +3,10 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'register',
-    loadComponent: () => import('./register/register.page').then( m => m.RegisterPage)
+    loadComponent: () => import('./auth/ui/pages/register/register.page').then( m => m.RegisterPage)
   },
   {
+<<<<<<< Updated upstream
     path: 'tabs',
     loadComponent: () => import('./shared/ui/pages/tabs/tabs.page').then( m => m.TabsPage)
   },
@@ -15,5 +16,10 @@ export const routes: Routes = [
     
   }
 
+=======
+    path: 'login',
+    loadComponent: () => import('./auth/ui/pages/login/login.page').then( m => m.LoginPage)
+  },
+>>>>>>> Stashed changes
 
 ];
--- pulhapanzak-app/src/app/auth/ui/pages/register/register.page.ts
+++ pulhapanzak-app/src/app/auth/ui/pages/register/register.page.ts
@@ -1,7 +1,8 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { User } from '../Models/IUser';
+import { RouterLink } from '@angular/router';
+import { User } from '../../../../Models/IUser';
 import {
   IonContent,
   IonHeader,
@@ -12,6 +13,8 @@ import {
   IonInput,
   IonButton,
   IonIcon,
+  IonRouterLink,
+  IonInputPasswordToggle
 } from '@ionic/angular/standalone';
 
 @Component({
@@ -31,7 +34,10 @@ import {
     IonButton,
     IonIcon,
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterLink,
+    IonRouterLink,
+    IonInputPasswordToggle
   ]
 })
 export class RegisterPage {
@@ -39,37 +45,26 @@ export class RegisterPage {
 
   constructor(private fb: FormBuilder) {
     this.registerForm = this.fb.group({
-      fullName: ['', Validators.required],
+      firstName: ['', Validators.required],
       lastName: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
       identityNumber: ['', [Validators.required, Validators.minLength(13), Validators.pattern(/^\d+$/)]],
-      phoneNumber: ['', [Validators.required, Validators.minLength(8), Validators.pattern(/^\d+$/)]],
-      birthDate: ['', Validators.required]
+      phoneNumber: ['', [Validators.required, Validators.minLength(8), Validators.pattern(/^\d+$/)]]
     });
   }
 
-  get fullName() { return this.registerForm.get('fullName')!; }
-  get lastName() { return this.registerForm.get('lastName')!; }
-  get email() { return this.registerForm.get('email')!; }
-  get password() { return this.registerForm.get('password')!; }
-  get identityNumber() { return this.registerForm.get('identityNumber')!; }
-  get phoneNumber() { return this.registerForm.get('phoneNumber')!; }
-  get birthDate() { return this.registerForm.get('birthDate')!; }
+  get isFirstNameInvalid(): boolean { return this.registerForm.get('firstName')!.invalid && (this.registerForm.get('firstName')!.dirty || this.registerForm.get('firstName')!.touched); }
+  get isLastNameInvalid(): boolean { return this.registerForm.get('lastName')!.invalid && (this.registerForm.get('lastName')!.dirty || this.registerForm.get('lastName')!.touched); }
+  get isEmailInvalid(): boolean { return this.registerForm.get('email')!.invalid && (this.registerForm.get('email')!.dirty || this.registerForm.get('email')!.touched); }
+  get isPasswordInvalid(): boolean { return this.registerForm.get('password')!.invalid && (this.registerForm.get('password')!.dirty || this.registerForm.get('password')!.touched); }
+  get isIdentityNumberInvalid(): boolean { return this.registerForm.get('identityNumber')!.invalid && (this.registerForm.get('identityNumber')!.dirty || this.registerForm.get('identityNumber')!.touched); }
+  get isPhoneNumberInvalid(): boolean { return this.registerForm.get('phoneNumber')!.invalid && (this.registerForm.get('phoneNumber')!.dirty || this.registerForm.get('phoneNumber')!.touched); }
 
   onSubmit() {
     if (this.registerForm.valid) {
       const user: User = this.registerForm.value;
       console.log('User Registered:', user);
     }
-  }
-
-  allowOnlyNumbers(event: KeyboardEvent): boolean {
-    const charCode = event.charCode ? event.charCode : event.keyCode;
-    if (charCode < 48 || charCode > 57) {
-      event.preventDefault();
-      return false;
-    }
-    return true;
   }
 }
