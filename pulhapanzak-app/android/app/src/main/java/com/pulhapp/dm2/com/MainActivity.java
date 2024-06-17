d831326aaf543eb8f6842e45dc8e4bce97524593
HaroruDa3 <harol.kock34@gmail.com>
2024-06-16 18:54:36 -0600

    X
    X

---
+++ pulhapanzak-app/android/app/src/main/java/com/pulhapp/dm2/com/MainActivity.java
@@ -0,0 +1,5 @@
+package com.pulhapp.dm2.com;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- pulhapanzak-app/capacitor.config.ts
+++ pulhapanzak-app/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.pulhapp.dm2.com',
   appName: 'pulhapanzak-app',
   webDir: 'www'
 };
